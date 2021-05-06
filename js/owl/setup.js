a13607b251776483a03a6fe0d56f478d0f4eae51
Rodrigo Lima <digo-pe@hotmail.com>
2021-05-06 01:25:58 -0300

    commit clone netflix - warflix (DIO)

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        650:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
